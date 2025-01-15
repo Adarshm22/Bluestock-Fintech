6224c1e38a5080998106537637d095f29c8d4f56
sunny-yadav <sunny9405683@gmail.com>
2025-01-15 23:02:07 +0530

    added a error handelling middleware

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 import { connectDB } from "./db/index.js";
+import { ErrorHandeller } from "./Utils/ErrorHandeller.js";
 
 dotenv.config({
   path: "../.env",
 });
 
+app.use(ErrorHandeller)
 const PORT = process.env.PORT || 8000;
 
 connectDB()
