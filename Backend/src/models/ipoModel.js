afee0306e2fb0cfde9fb642c79c8d8c6666ab395
Cout_dev <shubhankarrai007@gmail.com>
2025-01-17 16:47:49 +0530

    models

---
+++ Backend/src/models/ipoModel.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+
+const ipoSchema = new mongoose.Schema({
+ 
+  companyLogoURL: {
+    type: String,
+    required: true, 
+  },
+  companyName: {
+    type: String,
+    required: true, 
+  },
+  priceBand: {
+    type: String,
+    required: true, 
+  },
+  open: {
+    type: Date,
+    required: true, 
+  },
+  close: {
+    type: Date,
+    required: true, 
+  },
+  issueSize: {
+    type: String,
+    required: true, 
+  },
+  issueType: {
+    type: String,
+    required: true, 
+  },
+  listingDate: {
+    type: Date,
+    required: true, 
+  },
+  status: {
+    type: String,
+    enum: ['Upcoming', 'Ongoing', 'Closed', 'Listed'], 
+    required: true,
+  },
+  ipoPrice: {
+    type: Number,
+    required: true, 
+  },
+  listingPrice: {
+    type: Number,
+    required: true, 
+  },
+  listingGain: {
+    type: Number, 
+    required: true,
+  },
+  cmp: {
+    type: Number, 
+    required: true,
+  },
+  currentReturn: {
+    type: Number, 
+    required: true,
+  },
+  rhpPdfLink: {
+    type: String, 
+    required: true,
+  },
+  drhpPdfLink: {
+    type: String, 
+    required: true,
+  },
+});
+
+const ipoModel = mongoose.model('IPO', ipoSchema);
+
+module.exports = ipoModel;
