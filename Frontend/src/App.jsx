835312846b7e6c62b6df211b1fd3f408d0d58e9c
sunny-yadav <sunny9405683@gmail.com>
2025-02-17 00:04:27 +0530

    added the routing for the pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,26 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import LoginForm from './components/AdminSidePages/Forms/Login';
+import SignUpForm from './components/AdminSidePages/Forms/SignUp'
+import ForgotPassword from './components/AdminSidePages/Forms/ForgotPassword';
+import IpoForm from './components/AdminSidePages/Forms/RegisterIpo';
+import UpcomingIpoDashboard from './components/AdminSidePages/IpoPages/UpcomingIpo';
+import QuickLinks from './components/AdminSidePages/IpoPages/Dashboard';
+import Container from './components/UserSidePages/Container';
+import DashboardLayout from './components/AdminSidePages/DashboardContainer';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path='/'  />
+      <Route path='/' element={<Container />} />
+      <Route path='/signUp' element={<SignUpForm />} />
+      <Route path='/signIn' element={<LoginForm />} />
+
+      <Route path='/dashboard' element={<DashboardLayout />}>
+        <Route path='/dashboard/' element={<QuickLinks />} />
+        <Route path='/dashboard/manage-ipo' element={<UpcomingIpoDashboard />} />
+        <Route path='/dashboard/manage-ipo/register-ipo' element={<IpoForm />} />
+      </Route>
+
     </>
   )
 )
