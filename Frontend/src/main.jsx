7e92bea0290457908fe3f498b29f4ec2d5b777fe
sunny-yadav <sunny9405683@gmail.com>
2025-02-17 23:51:29 +0530

    created a auth provider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,14 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'react-hot-toast'
+import { AuthContextProvider } from './context/authContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-    <>
+    <AuthContextProvider>
         <App />
         <Toaster
             position="top-right"
         />
-    </>
+    </AuthContextProvider>
 
 )
