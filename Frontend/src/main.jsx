ceedb5e5e9c4eaafd556233ee98739b76a92802e
sunny-yadav <sunny9405683@gmail.com>
2025-02-18 15:00:35 +0530

    added the context for ipo

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,14 +3,17 @@ import './index.css'
 import App from './App.jsx'
 import { Toaster } from 'react-hot-toast'
 import { AuthContextProvider } from './context/authContext.jsx'
+import { IpoContextProvider } from './context/ipoContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 
     <AuthContextProvider>
-        <App />
-        <Toaster
-            position="top-right"
-        />
+        <IpoContextProvider>
+            <App />
+            <Toaster
+                position="top-right"
+            />
+        </IpoContextProvider>
     </AuthContextProvider>
 
 )
